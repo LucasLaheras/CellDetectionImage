04aab7592adb193aa2317006bb4b8319ffccca9c
THERMALTAKE <lucaslaheras@hotmail.com>
2019-01-21 19:08:21 -0200

    psrGrayHistogram add

--- CellDetectionImage.py
+++ CellDetectionImage.py
@@ -4,25 +4,24 @@ def CellDetectionImage(im0):
 
     im1 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
 
-    im1 = [im1],[0],None,[256],[0,256]
+    im1 = cv2.calcHist([im1],[0],None,[256],[0,256])
 
-    for i in im1:
-        print(str(i) + ' ')
+    H = psrGrayHistogram(im1)
+
+    print(str(H))
 
     return
 
-"""
+
 def psrGrayHistogram(im):
 
-    [Lin, Col] = size(im)
-    H = zeros(1,256)
-    for y=1:Lin
-        for x=1:Col
-          H(im(y,x)+1) = H(im(y,x)+1) + 1
-        end
-    end
-    % normalization
+    Lin, Col= im.shape
+    H = []
+    for i in range(256):
+        H.append(0)
+    for y in range(1,Lin+1):
+        for x in range(1, Col+1):
+            H[im[y, x]+1] = H[im[y, x]+1] + 1
+    # normalization
     H = H/(sum(H))
     return H
-
-"""
